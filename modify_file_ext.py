ec10ff5f7e79ab2162218ed0240d71c080e4c5f2
libl17 <37764721+libl17@users.noreply.github.com>
2022-07-02 21:59:03 +0800

    Delete modify_file_ext.py

--- modify_file_ext.py
+++
@@ -1,7 +0,0 @@
-import os
-if __name__ == '__main__':
-    path = './data/images/'
-    imgs = os.listdir(path)
-    for img in imgs:
-        img_mod = img.replace('.JPG','.jpg')
-        os.rename(path + img, path + img_mod)
